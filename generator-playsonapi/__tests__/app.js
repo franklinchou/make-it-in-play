fa552512b350865315a4daf084178cbd4a9cf659
Franklin Chou <franklin@peeriq.com>
2018-10-28 18:13:28 -0400

    make pretty

--- generator-playsonapi/__tests__/app.js
+++ generator-playsonapi/__tests__/app.js
@@ -5,12 +5,19 @@ const helpers = require('yeoman-test');
 
 describe('generator-playsonapi:app', () => {
   beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      name: 'test',
+      organization: 'com.test',
+      projectVersion: '0.0.1',
+      sbtVersion: global.sbtVersion,
+      playJsonExtensionsVersion: global.playJsonExtensionsVersion,
+      scalaTestVersion: global.scalaTestVersion,
+      scalaTestPlusPlayVersion: global.scalaTestPlusPlayVersion,
+      typesafeSbtPluginVersion: global.typesafeSbtPluginVersion
+    });
   });
 
   it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+    assert.file(['build.sbt']);
   });
 });
